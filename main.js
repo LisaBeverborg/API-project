95a97fb2a1c6b7671c355836e7674eec0ca32146
iths-lisa-beverborg <54471572+iths-lisa-beverborg@users.noreply.github.com>
2019-10-18 19:48:09 +0200

    update
    added css
    release date string shortened

--- main.js
+++ main.js
@@ -1,51 +1,54 @@
 //show author name on click book list
-
-
 async function getData(){
     let response = await fetch ( "https://www.anapioficeandfire.com/api/books/")
     let responseBody = await response.json()
     return responseBody
-  
 }
-
-getData ()
-
 async function render(){
-    console.log("render called")
     let list = await getData()
-
-    let ul = document.querySelector("ul")
-
+    let ul = document.querySelector("ul") 
     let itemPrototype = document.querySelector ("li.prototype")
-        for (let item of list){
-
-    let newItem= itemPrototype.cloneNode(true)
-    newItem.classList.remove("prototype")
-    
-
-    newItem.addEventListener ("click", async function(){
-        document.body.querySelector(".authors").innerText = item.authors[0]
-        document.body.querySelector(".pages").innerText = item.numberOfPages
-        document.body.querySelector(".released").innerText = item.released
-        document.body.querySelector(".characters").innerText = item.povCharacters[0]
-       
-
-        item.povCharacters
-        
-        //document.body.querySelector(".povcharacters").innerText = item.povCharacters
-        //let response =  await getDataCharacter()
-        //let responseBody = await response.json();
-
-    })
-    let  p=newItem.querySelector("p")
-    p.innerText = item.name
-   
-        ul.append (newItem)
-    
-
+    for (let item of list){
+        let newItem = document.createElement("li")
+        let para = document.createElement("h1")
+        para.classList.add("name")
+        newItem.append(para)
+        newItem.classList.remove("prototype")
+        newItem.addEventListener ("click", async function(){
+            document.body.querySelector(".authors").innerText = "Author: " + item.authors[0]
+            document.body.querySelector(".pages").innerText = "Pages: " + item.numberOfPages 
+            let date = strDate(item.released, 9, 0)
+            document.body.querySelector(".released").innerText = "Release date: " + date
+           
+            let response = await fetch(personURL)
+            let charData = await response.json()
+            
+           
+            let newThing = document.createElement("div")
+            let newTwo = document.createElement("h3")
+            newTwo.classList.add("povcharacters")
+            newThing.append(newTwo) 
+            let listChar = await (personURL)
+            for (let personURL of listChar){     
+               
+                return charData
+            }
+                document.body.querySelector(".characters").innerText = "Name: " + item.povCharacters.name 
+                 
+        })
+        let  p=newItem.querySelector("h1")
+        p.innerText = item.name
+        ul.append (newItem)  
     }
 }
 
+function strDate (oldDate, stop, start){
+    let newDate = ""
+    for (i=start; i<=stop; i++){
+        newDate += oldDate[i]
+    }
+    return newDate
+}
 
 render()
 
